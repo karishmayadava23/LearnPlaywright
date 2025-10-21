3e106e47634ffe0c3a99c8ca042c2c7ed3b86671
karishma <karishma.qaengr@gmail.com>
2025-10-21 21:05:14 +0530

    full assertion completed

--- tests/learnAssertions.spec.js
+++ tests/learnAssertions.spec.js
@@ -39,7 +39,7 @@ test('AssertionTest', async ({ page }) => {
 
   // 5. await expect(locator).toBeChecked
   //checkbox
-  // await page.goto("https://letcode.in/radio");
+  // await page.goto("https://letcode.in/test");
   // const checkbox1 = page.locator('.card-content .checkbox').nth(0);
   // await expect(checkbox1).toBeChecked();
 
@@ -49,7 +49,33 @@ test('AssertionTest', async ({ page }) => {
   // await expect(radiobuttonnotfoo).toBeChecked();
   // await expect(radiobuttonfoo).toBeChecked();
 
+  //6.  await expect(locator).toHaveAttribute()- Element has a DOM attribute
+  // const yesRadiobutton = await page.locator('#yes');
+  // await expect(yesRadiobutton).toHaveAttribute('type', 'radiol');
+
+  // 7. await expect(locator).toContainText()- Element contains text 
+  //await expect(locator).toHaveText()- Element matches text
+  // await page.goto("https://letcode.in/button");
+  // const subTitleLocator = await page.locator('.container .subtitle');
+  // // await expect(subTitleLocator).toHaveText('Empowering Test Automation Engineers through learning');
+  //await expect(subTitleLocator).toHaveText('Empowering Test Automation Engineers through learnings'); //incorrect text
+
+  // await expect(subTitleLocator).toContainText('Empowering');
+  // await expect(subTitleLocator).toContainText('Empowerin$$$$$$$$$$$$$$4'); //incorrect text
+
+  //8. await expect(locator).toHaveValue() -Input has a value
+  // await page.goto("https://letcode.in/edit");
+  // const fullName = await page.locator('#fullName');
+  // await fullName.fill('Karishma');
+  // // await expect(fullName).toHaveValue('Karishma');
+
+  // await expect(fullName).toHaveValue('Karishmaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
 
 
+  //9. await expect(locator).toHaveCount()	List has exact number of children
+  await page.goto("https://letcode.in/dropdowns");
+  const fruitList = await page.locator('#fruits option');
+  // await expect(fruitList).toHaveCount(6);
+  await expect(fruitList).toHaveCount(7);
 
 });
