8634507c7fdc3d4c11dd14c5ccbb34ea60126ffd
karishma <karishma.qaengr@gmail.com>
2026-03-03 12:46:08 +0530

    Handle Bootstrapdropdown

--- tests/HandleBootstrapDropdown.spec.js
+++ tests/HandleBootstrapDropdown.spec.js
@@ -3,8 +3,32 @@ const { test, expect } = require('@playwright/test');
 test("Handle BootStrapDropdown", async ({ page }) => {
     await page.goto("https://getbootstrap.com/docs/5.3/components/dropdowns/");
     await page.locator("#bd-theme").click();
+    // const themeOption = await page.locator("ul .dropdown-menu.show li button");
+    //expect(themeOption).toHaveCount(3);
+    //   await page.waitForTimeout(5000);
 
-    await page.waitForTimeout(5000);
+
+    //by array
+    const themeOption = await page.$$("ul .dropdown-menu.show li button");
+    const optionLength = themeOption.length;
+    expect(optionLength).toBe(3);
+
+
+    //select the option
+    for (let option of themeOption) {
+        console.log(await option.textContent());
+        const text = await option.textContent();
+        if (text.includes("Dark")) {
+            await option.click();
+
+        }
+
+        await page.waitForTimeout(5000);
+
+
+
+
+    }
 
 
 
